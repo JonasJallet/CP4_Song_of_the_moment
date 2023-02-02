cf5298220062964ce283e8a862f48de05386596a
JonasJallet <jonas.jallet@gmail.com>
2023-02-02 16:03:31 +0100

    complete login, logout, registration / fix css and other details

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class LoginController extends AbstractController
 {
@@ -22,5 +23,18 @@ class LoginController extends AbstractController
             'last_username' => $lastUsername,
             'error'         => $error,
         ]);
+    }
+
+    #[Route('/login/redirect', name: 'app_login_redirect')]
+    #[IsGranted('ROLE_USER')]
+    public function redirectAfterLogin(): Response
+    {
+        if (in_array('ROLE_ADMIN', $this->getUser()->getRoles())) {
+            return $this->redirectToRoute('app_song_index', [], Response::HTTP_SEE_OTHER);
+        }
+        if (in_array('ROLE_USER', $this->getUser()->getRoles())) {
+            return $this->redirectToRoute('app_song_list', [], Response::HTTP_SEE_OTHER);
+        }
+        return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
     }
 }
