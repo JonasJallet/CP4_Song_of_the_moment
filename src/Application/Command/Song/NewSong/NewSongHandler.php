42682b218d988d49246b70099581d0b17f154e1c
JonasJallet <jonas.jallet@gmail.com>
2023-08-19 17:12:07 +0200

    upload cover for manually add song

--- src/Application/Command/Song/NewSong/NewSongHandler.php
+++ src/Application/Command/Song/NewSong/NewSongHandler.php
@@ -4,12 +4,14 @@ namespace App\Application\Command\Song\NewSong;
 
 use App\Domain\Repository\DomainSongRepositoryInterface;
 use App\Domain\Service\LinkYoutubeFormatInterface;
+use App\Domain\Service\SongUploadCoverInterface;
 
 class NewSongHandler
 {
     public function __construct(
         public DomainSongRepositoryInterface $songRepository,
         public LinkYoutubeFormatInterface $linkYoutubeFormat,
+        public SongUploadCoverInterface $songUploadCover,
     ) {
     }
     public function __invoke(NewSong $newSong): void
@@ -18,6 +20,10 @@ class NewSongHandler
         $linkYoutube = $song->getLinkYoutube();
         $linkFormat = $this->linkYoutubeFormat->format($linkYoutube);
         $song->setLinkYoutube($linkFormat);
+        $name = $song->getArtist() . ' - ' . $song->getTitle() . '.avif';
+        $song->setPhotoAlbum(
+            $this->songUploadCover->upload($song->getPhotoAlbum(), $name)
+        );
         $this->songRepository->save($song, true);
     }
 }
